543e30bef6e7b28e072301a721600844d8d44142
Cole Peters <coledpeters@outlook.com>
2026-01-09 16:43:31 -0500

    error upload

--- HW8/MM1_HW9_p1.m
+++
@@ -1,9 +0,0 @@
-syms lam real
-A = [1 0 1 4;
-     1 0 2 -1;
-     2 1 3 -2];
-b = [2; 1; -3];
-
-xlam = simplify((A.'*A + lam*eye(4)) \ (A.'*b));  
-
-x_min_norm = simplify(limit(xlam, lam, 0, 'right')) ;
